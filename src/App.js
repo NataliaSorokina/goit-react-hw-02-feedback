baf03bc5343ca66964faf6fdb5e8284a468f75a1
Natalia Sorokina <pticasonabird@gmail.com>
2021-09-05 19:49:59 +0300

    done step 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import styles from './App.module.css';
+import Feedback from 'components/Feedback/Feedback';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Feedback initialGood={0} initialNeutral={0} initialBad={0} />
     </div>
   );
 }
